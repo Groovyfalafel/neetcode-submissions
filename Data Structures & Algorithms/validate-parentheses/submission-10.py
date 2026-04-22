0e5a8d6e074005a362019fd2133a12ef2107e981
Anas Mohammad <166111908+Groovyfalafel@users.noreply.github.com>
2026-04-22 02:09:29 -0400

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Base case
+        if len(s) < 2:
+            return False
+        
+        stack = []
+        for i in range(len(s)):
+
+            if s[i] == '(' or s[i] == '{' or s[i] == '[':
+                stack.append(s[i])
+
+            elif s[i] == ')':
+                if not stack or stack[-1] != '(':
+                    return False
+                stack.pop()
+
+            elif s[i] == '}':
+                if not stack or stack[-1] != '{':
+                    return False
+                stack.pop()
+
+            elif s[i] == ']':
+                if not stack or stack[-1] != '[':
+                    return False
+                stack.pop()
+
+
+        return len(stack) == 0
